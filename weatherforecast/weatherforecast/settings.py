953054575d084b7eb245d25281259e3c9b3972b8
hilkafi <kafi.bru@gmail.com>
2021-05-28 05:32:34 +0000

    setting changes

--- weatherforecast/weatherforecast/settings.py
+++ weatherforecast/weatherforecast/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-m3vrxjqg!jp#zqrsb8*4vr*!xyl-diq_)j=1mdcpb0p!0(yb_z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hilkafi.pythonanywhere.com']
 
 
 # Application definition
@@ -132,3 +133,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
